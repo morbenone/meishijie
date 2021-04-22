93c230f633dae23d8f79810cd690a03385b5cd39
xueyukeji/zhangjuan <648587841@qq.com>
2021-04-22 10:43:04 +0800

    删除history

--- .history/src/service/api_20210421145303.js
+++
@@ -1,7 +0,0 @@
-import {get ,getAwait} from './http'
-export function getLogin(username,password){
-    return get('/getLogin',username,password)
-}
-export function getUserInfo(param){
-    return getAwait('/getUser',param)
-}
